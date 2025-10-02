1bffe6bf6a5adbebab17110050375c60c0611fe5
Christian008062004 <cgrlanwer@gmail.com>
2025-10-02 11:12:01 +0200

    vite config anpassen

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
-// vite.config.ts
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import path from "path";
 
-export default defineConfig(({ command }) => ({
+export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: { "@": path.resolve(__dirname, "src") },
